6bc965da86a6ca2882bd26d45d0bbb28e87d29c2
SnowPhoenix <1595763343@qq.com>
2021-08-28 13:33:41 +0800

    init

---
+++ src/main/java/top/snowphonix/toolsetusercenter/ToolSetUserCenterApplication.java
@@ -0,0 +1,13 @@
+package top.snowphonix.toolsetusercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSetUserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolSetUserCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/snowphonix/toolsetusercenter/ToolSetUserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package top.snowphonix.toolsetusercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSetUserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
