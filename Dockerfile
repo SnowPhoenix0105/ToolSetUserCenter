9938551123bd4b74e5354ec0be307197beffeddb
SnowPhoenix0105 <1595763343@qq.com>
2021-09-28 20:36:57 +0800

    add health/get api

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@
 FROM openjdk:8-jre
 WORKDIR /usr/local
 COPY ./target/ToolSetUserCenter.jar /usr/local/ToolSetUserCenter.jar
-CMD ["java", "-jar", "/user/local/ToolSetUserCenter.jar"]
+ENTRYPOINT ["java", "-jar", "/user/local/ToolSetUserCenter.jar"]
---
+++ src/main/java/top/snowphonix/toolsetusercenter/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package top.snowphonix.toolsetusercenter.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping("get")
+    public String get() {
+        return "success!";
+    }
+}
